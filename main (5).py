6a744c130fb500791c84a704e65b4600348c0ee3
mohamedkathim149 <144907188+mohamedkathim149@users.noreply.github.com>
2023-10-11 21:15:16 +0530

    Add files via upload

---
+++ main (5).py
@@ -0,0 +1,14 @@
+i=1
+fact=1
+n=int(input("Enter the number:"))
+while(i<=n):
+fact fact*i
+i=i+1
+print(fact)
+
+
+year=int(input("Enter the year:"))
+if(year%4==0):
+ print( year, "is leap year")
+else:
+ print( year, "Non leap year")
